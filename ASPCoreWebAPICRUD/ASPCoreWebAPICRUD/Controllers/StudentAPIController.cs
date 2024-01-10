5a3f50a638c249d44b0e330da03e3c1a5784a447
Muhammad-Kashi <skashif394@gmail.com>
2024-01-10 15:05:07 +0500

    .net Core CRUP Web API

---
+++ ASPCoreWebAPICRUD/ASPCoreWebAPICRUD/Controllers/StudentAPIController.cs
@@ -0,0 +1,78 @@
+﻿using ASPCoreWebAPICRUD.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPCoreWebAPICRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+        private readonly CodeFirstDBContext context;
+
+        // CodeFirstDBContext is a database name
+        public StudentAPIController(CodeFirstDBContext context)
+        {
+            this.context = context;
+        }
+
+        //Get All Students
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> GetStudents()
+        {
+            var data = await context.Students.ToListAsync();
+            return Ok(data);
+        }
+
+        //GET SINGLE Students
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudentById(int id)
+        {
+            var student = await context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return student;
+        }
+
+
+        //GET SINGLE Students
+        [HttpPost]
+        public async Task<ActionResult<Student>> CreateStudent(Student std)
+        {
+            await context.Students.AddAsync(std);
+            await context.SaveChangesAsync();
+
+            return Ok(std);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Student>> UpdateStudent(int id,Student std)
+        { 
+            if(id != std.Id)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(std).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            
+            return Ok(std);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Student>> DeleteStudent(int id)
+        {
+            var std = await context.Students.FindAsync(id);
+            if(std == null)
+            {
+                return BadRequest();
+            }
+            context.Students.Remove(std);
+            await context.SaveChangesAsync();
+            return Ok(std);
+        }
+    }
+    }
---
+++ ASPCoreWebAPICRUD/ASPCoreWebAPICRUD/Models/CodeFirstDBContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ASPCoreWebAPICRUD.Models
+{
+    // here CodeFirstDBContext is a database Name
+    public partial class CodeFirstDBContext : DbContext
+    {
+        public CodeFirstDBContext()
+        {
+        }
+
+        public CodeFirstDBContext(DbContextOptions<CodeFirstDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            { 
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.FatherName)
+                    .HasMaxLength(1000)
+                    .IsUnicode(false)
+                    .HasDefaultValueSql("('')");
+
+                entity.Property(e => e.StudentGender)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StudentName)
+                    .HasMaxLength(1000)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
